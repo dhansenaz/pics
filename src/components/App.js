965f518ca7a742bd6801c0cf1571551ab023fe30
dhansenaz <dhansen@allthingsdot.com>
2019-03-04 07:09:44 -0800

    async await and binding

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,26 @@ import axios from 'axios';
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
+
+    state = {
+        images: []
+    }
+  onSearchSubmit = async (term) => {
      
-    axios.get('https://api.unsplash.com/search/photos/',{
+    const response = await axios
+    .get('https://api.unsplash.com/search/photos/',{
         params: { query: term},
         headers: {
         Authorization: 'Client-ID e19041b6c2e5367ecab45c28001595b66e54583946a109fe9a465375d6f7c60b'
     }
     })
+    this.setState({images: response.data.results});
   }
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
